ac5f51f95bceb5bd6a3bf5ee68798026270f9b9c
Xuanita <alabama10@inbox.lv>
2024-01-16 23:35:54 +0200

    feat: add 02-write-file solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
+
+// Создание потока записи в файл
+const outputStream = fs.createWriteStream(path.join(__dirname, 'output.txt'));
+
+// Создание интерфейса readline
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Введите текст (или введите exit для выхода):');
+
+// Обработка ввода пользователя
+rl.on('line', (input) => {
+  if (input.trim() === 'exit') {
+    rl.close();
+  } else {
+    outputStream.write(input + '\n');
+  }
+});
+
+// Обработка закрытия readline (например, при вводе 'exit')
+rl.on('close', () => {
+  console.log('Завершение записи, выход из программы.');
+  outputStream.close();
+});
+
+// Обработка принудительного завершения процесса (Ctrl + C)
+process.on('SIGINT', () => {
+  console.log('Принудительное завершение процесса.');
+  rl.close();
+  outputStream.close();
+});
