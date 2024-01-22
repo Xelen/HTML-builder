4397dcb5ec8dccfd58a49668a57fa8a95e4d5d93
Xuanita <alabama10@inbox.lv>
2024-01-22 16:23:03 +0200

    refactor: delete comments

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,22 +1,22 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-const stylesDir = path.join(__dirname, 'styles'); // путь к директории с файлами css
-const outputFilePath = path.join(__dirname, 'project-dist', 'bundle.css'); // путь к файлу с общими стилями в новой диреткории
+const stylesDir = path.join(__dirname, 'styles');
+const outputFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
 
 async function mergeStyles() {
   try {
     const files = await fs.readdir(stylesDir); // читаем файлы из первой директории, получаем список файлов
     const cssFiles = files.filter((file) => path.extname(file) === '.css'); // выбираем только файлы с расширением css и складываем в cssFiles
 
-    let styles = ''; // задаем пустую переменную, куда сгрузим все содержимое файлов из cssFiles
+    let styles = '';
     for (const file of cssFiles) {
       //проход по каждому файлу
-      const filePath = path.join(stylesDir, file); // берем путь на файл
-      const data = await fs.readFile(filePath, 'utf8'); // читаем этот файл
-      styles += data + '\n'; // добавляем содержимое  в переменную
+      const filePath = path.join(stylesDir, file);
+      const data = await fs.readFile(filePath, 'utf8');
+      styles += data + '\n';
     }
-    // console.log(styles);
+
     await fs.writeFile(outputFilePath, styles); //пишем содержимое переменной styles  в новый файл
     console.log('Styles merged into bundle.css');
   } catch (err) {
@@ -24,4 +24,4 @@ async function mergeStyles() {
   }
 }
 
-mergeStyles(); //вызываем функцию
+mergeStyles();
